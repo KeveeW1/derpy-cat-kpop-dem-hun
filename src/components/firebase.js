9b4eb2f903163829f4dc4c1109fae64a1f7a99d8
Keira <1keira.kw@gmail.com>
2025-07-07 20:51:08 -0700

    commented out firebase analytics. site crashed lol

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,7 +1,7 @@
 // firebase.js
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
-import { getAnalytics } from "firebase/analytics";
+//import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -15,4 +15,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-const analytics = getAnalytics(app);
+//const analytics = getAnalytics(app);
