d42589a0a6f1d74effe5ed34cd0dbee197a2f0b0
Keira <1keira.kw@gmail.com>
2025-07-07 23:08:28 -0700

    fixed leaderboard. updates in real-time. better firebase rule security. attempt 3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import TigerClicker from './components/TigerClicker';
 import './App.css';
+import { Analytics } from '@vercel/analytics/react';
+
 
 function App() {
   return (
     <div className="App">
       <TigerClicker />
+      <Analytics />
     </div>
   );
 }
